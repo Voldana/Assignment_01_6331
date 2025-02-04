52bb286e5dd4f853fc6d6a11834d5af727740a9e
Mohammad Darandeh <m.darande@gmail.com>
2025-02-04 11:24:38 -0500

    Create base classes

---
+++ Assets/Scripts/Environment/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Environment
+{
+    public class Obstacle : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("TradeShip") || other.CompareTag("PirateShip"))
+            {
+                Debug.Log(other.name + " collided with an obstacle!");
+            }
+        }
+    }
+}
--- Assets/Scripts/Ships/Base.cs
+++ Assets/Scripts/Ships/Base.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
 
-public class Base : MonoBehaviour
+namespace Ships
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class Base : MonoBehaviour
     {
-        
-    }
+        public float maxSpeed = 5f;
+        public float acceleration = 2f;
+        public float rotationSpeed = 2f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        protected Vector3 velocity;
+        protected Vector3 targetPosition;
+
+        public virtual void SetTarget(Vector3 target)
+        {
+            targetPosition = target;
+        }
+
+        protected void Move()
+        {
+            Vector3 direction = (targetPosition - transform.position).normalized;
+            velocity += direction * acceleration * Time.deltaTime;
+            velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+            transform.position += velocity * Time.deltaTime;
+            transform.up = velocity.normalized; // Face movement direction
+        }
     }
 }
