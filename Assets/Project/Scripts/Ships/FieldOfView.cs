44d48268f2fc5fe20342ab1ff5090a93d26f500a
Mohammad Darandeh <m.darande@gmail.com>
2025-02-06 18:35:13 -0500

    Create FOV

---
+++ Assets/Project/Scripts/Ships/FieldOfView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Project.Scripts.Ships
+{
+    public class FieldOfView : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log(other.gameObject.tag);
+        }
+    }
+}
--- Assets/Project/Scripts/Ships/Pirate.cs
+++ Assets/Project/Scripts/Ships/Pirate.cs
@@ -7,31 +7,16 @@ namespace Project.Scripts.Ships
         [SerializeField] private float fieldOfViewAngle = 120f;
         [SerializeField] private float detectionRange = 15f;
 
-        public Transform islandCenter;
-        private Trade targetTradeShip;
-        private bool isChasing = false;
-
-        /*private void Update()
+        /*void Start()
         {
-            if (isChasing && targetTradeShip != null)
-            {
-                SetTarget(targetTradeShip.transform.position);
-            }
-            else
-            {
-                WanderAroundIsland();
-            }
+            gameObject.AddComponent<SteeringController>();
+        
+            Wander wander = gameObject.AddComponent<Wander>();
+            wander.island = island;
 
-            Move();
+            Pursue pursue = gameObject.AddComponent<Pursue>();
+            pursue.target = tradeShip;
         }
-
-        private void WanderAroundIsland()
-        {
-            if (!(Vector3.Distance(transform.position, targetPosition) < 2f)) return;
-            float randomAngle = Random.Range(0, 360);
-            Vector3 offset = new Vector3(Mathf.Cos(randomAngle), Mathf.Sin(randomAngle), 0) * 10;
-            SetTarget(islandCenter.position + offset);
-        }*/
 
         private void OnTriggerEnter(Collider other)
         {
@@ -49,18 +34,6 @@ namespace Project.Scripts.Ships
                 isChasing = false;
                 targetTradeShip = null;
             }
-        }
-
-        private void OnDrawGizmos()
-        {
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(transform.position, detectionRange);
-
-            var leftLimit = Quaternion.Euler(0, 0, fieldOfViewAngle / 2) * transform.up * detectionRange;
-            var rightLimit = Quaternion.Euler(0, 0, -fieldOfViewAngle / 2) * transform.up * detectionRange;
-
-            Gizmos.DrawLine(transform.position, transform.position + leftLimit);
-            Gizmos.DrawLine(transform.position, transform.position + rightLimit);
-        }
+        }*/
     }
 }
